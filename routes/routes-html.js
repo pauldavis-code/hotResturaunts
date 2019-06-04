38b28b62f9e87d50ad0a104689ddc216b69707da
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-04 19:38:47 -0400

    added html routes

--- routes/routes-html.js
+++ routes/routes-html.js
@@ -9,6 +9,10 @@ module.exports = function(app) {
   })
 
   app.get('/tables', function(req, res) {
-    res.sendFIle(path.jpoin(__dirname + 'tables.html'))
+    res.sendFIle(path.join(__dirname + 'tables.html'))
+  })
+
+  app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname + 'reserve.html'))
   })
 }
